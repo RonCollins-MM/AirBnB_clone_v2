aeb56eaeba73b73ebc8442f860b47b3d617e259e
Roncollins MM <muriukiron@gmail.com>
2022-10-31 01:00:25 +0300

    Using Fabric to send compressed deploy webstatic to servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
+
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['54.174.153.154', '18.207.234.225i']
+
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split('/')[-1]
+        no_ext = file_n.split('.')[0]
+        path = '/data/web_static/releases/'
+        put(archive_path, '/tmp/')
+        run(f'mkdir -p {path}{no_ext}/')
+        run(f'tar -xzf /tmp/{file_n} -C {path}{no_ext}/')
+        run(f'rm /tmp/{file_n}')
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run(f'rm -rf {path}{no_ext}/web_static')
+        run('rm -rf /data/web_static/current')
+        run(f'ln -s {path}{no_ext}/ /data/web_static/current')
+        return True
+    except:
+        return False
