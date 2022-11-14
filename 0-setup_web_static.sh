c2f0685fdbe9c3c5b30deb0e98273767897d766e
Roncollins MM <muriukiron@gmail.com>
2022-11-14 16:39:52 +0300

    Preparing webservers to serve static content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,47 +1,69 @@
 #!/usr/bin/env bash
-# installs and configures a server to serve aribnb clone
+# sets up web server for deployment of web_static
+
+echo -e "\e[1;32m START\e[0m"
+#--Update packages
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
+echo -e "\e[1;32m Packages updated\e[0m"
+echo
+
 sudo ufw allow 'Nginx HTTP'
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+sudo -e "\e[1;32m Allow incoming NGINX HTTP connections\e[0m"
+echo
+
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo -e "\e[1;32m directories created"
+echo "<h1>Welcome to www.daktari.tech</h1>" > /data/web_static/releases/test/index.html
+echo -e "\e[1;32m Test content added\e[0m"
 
 if [ -d "/data/web_static/current" ];then
-    echo "path /data/web_static/current exists"
-    sudo rm -rf /data/web_static/current;
+	echo "path /data/web_static/current exists"
+	sudo rm -rf /data/web_static/current;
 fi;
+echo -e "\e[1;32m prevent overwrite\e[0m"
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-echo -e "<html>\n\
-     <head>\n\
-     </head>\n\
-     <body>\n\
-	This is Machiestay's domanin, you are welcome\n\
-     </body>\n\
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo chmod g+w /data/web_static/releases/test/index.html
-echo -e "server {\n\
-    listen 80 default_server;\n\
-    listen [::]:80 default_server;\n\
-    add_header X-Served-By $HOSTNAME;\n\
-    root /var/www/html;\n\n\
-    index index.html;\n\n\
-    location /hbnb_static/ {\n\
-    	     alias /data/web_static/current/;\n\
-	     index index.html index.htm;\n\
-    }\n\
-    location /redirect_me {\n\
-        return 301 http://cuberule.com/;\n\
-    }\n\
-    error_page 404 /404.html;\n\
-    location /404 {\n\
-      root /var/www/html;\n\
-      internal;\n\
-    }\n\
-}" | sudo tee /etc/nginx/sites-available/default > /dev/null
-sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
-sudo service nginx restart
+sudo chown -hR ubuntu:ubuntu /data
+
+new_config=\
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        
+	root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+	add_header X-Served-By \$hostname;
+        location / {
+                try_files \$uri \$uri/ =404;
+        }
+
+        location /hbnb_static {
+            alias /data/web_static/current;
+            index index.html index.htm;
+        }
+
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/user/Computerphile permanent;
+        }
+}
+"
+echo "Ceci n'est pas une page" > /var/www/html/404.html
+echo "$new_config" > /etc/nginx/sites-available/default
+
+if [ "$(pgrep -c nginx)" -le 0 ];
+then
+    service nginx start
+    echo -e "\e[1;32m start NGINX\e[0m"
+else
+    service nginx restart
+    echo -e "\e[1;32m restart NGINX\e[0m"
+fi;
