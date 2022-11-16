b5b11e87c0200624edc87b2c46c2ba3bd354c92b
Roncollins MM <muriukiron@gmail.com>
2022-11-16 10:24:38 +0300

    Routing: type checking

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+Simple Flask web application, localhost port:5000
 """
 
 from flask import Flask
@@ -9,32 +9,36 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """prints Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """prints HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+    """
+    Print 'C' followed by the value of the `text` variable
+    Underscores replaced by whitespaces.
+    """
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+    """Print 'Python', followed by the value of the `text` variable"""
     return 'Python ' + text.replace('_', ' ')
 
-
 @app.route('/number/<int:n>', strict_slashes=False)
-def imanumber(n):
-    """display “n is a number” only if n is an integer"""
+def iamanumber(n):
+    """
+    Prints 'n is a number' only if `n` is an integer
+    """
     return "{:d} is a number".format(n)
 
 if __name__ == '__main__':
