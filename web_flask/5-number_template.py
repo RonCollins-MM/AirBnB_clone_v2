fd756e70a18ce7cd0b2836d4fb572c65e7cb215c
Roncollins MM <muriukiron@gmail.com>
2022-11-16 10:30:38 +0300

    Jinja templating with flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,46 +1,49 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+Simple Flask web application, localhost port:5000
 """
 
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """prints Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """prints HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+    """
+    Print 'C' followed by the value of the `text` variable
+    Underscores replaced by whitespaces.
+    """
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+    """Print 'Python', followed by the value of the `text` variable"""
     return 'Python ' + text.replace('_', ' ')
 
-
 @app.route('/number/<int:n>', strict_slashes=False)
-def imanumber(n):
-    """display “n is a number” only if n is an integer"""
+def iamanumber(n):
+    """
+    Prints 'n is a number' only if `n` is an integer
+    """
     return "{:d} is a number".format(n)
 
-
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbersandtemplates(n):
-    """display a HTML page only if n is an integer"""
+def numberandtemplates(n):
+    """Renders an HTML page only if `n` is an integer """
     return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
