96a8d7122cf00138ed9e3f4a74b90b4765f79a07
Roncollins MM <muriukiron@gmail.com>
2022-11-16 10:12:57 +0300

    Routing to produce different messages

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+Simple Flask web app, localhost port:5000
 """
 
 from flask import Flask
@@ -9,13 +9,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """Prints Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """Prints HBNB"""
     return 'HBNB'
 
 if __name__ == '__main__':
